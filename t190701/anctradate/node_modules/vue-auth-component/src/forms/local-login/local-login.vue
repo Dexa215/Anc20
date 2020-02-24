4de3a4bd08cb05fee57b47b44476bf7d08dafce3
Mauro <dexa@hotmail.it>
2020-02-24 12:53:26 +0100

    Inizializzazione - 100 - AncTRadate App - 50 - NODE Modules

---
+++ t190701/anctradate/node_modules/vue-auth-component/src/forms/local-login/local-login.vue
@@ -0,0 +1,91 @@
+<template>
+<form @submit.prevent="formSubmitted({username, password})" class='vue-auth-component-form'>
+  <div v-if="error" class='form-error'>{{error.message}}</div>
+
+  <input v-model="username" :placeholder="usernamePlaceholder" tabIndex='1' />
+  <input v-model="password" :placeholder="passwordPlaceholder" tabIndex='1' type='password' />
+
+  <div class='forgot-password'>
+    <a href='javascript://' @click.prevent={forgotClicked} tabIndex='2'>forgot password</a>
+  </div>
+
+  <button type='submit' tabIndex='1'>{{buttonText}}</button>
+</form>
+</template>
+
+<script>
+import { sharedMethods, sharedProps } from '../forms';
+
+export default {
+  name: 'local-signup',
+  data () {
+    return {
+      error: undefined,
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    ...sharedMethods
+  },
+  props: {
+    ...sharedProps,
+    buttonText: {
+      type: String,
+      default: 'Login'
+    }
+  }
+}
+</script>
+
+<style lang="less">
+div.auth-ui-container {
+	form.vue-auth-component-form {
+		margin-bottom: 3px;
+	}	
+}
+
+form.vue-auth-component-form {
+	text-align: center;
+	font-size: 16px;
+	margin-top: 22px;
+
+	div.forgot-password {
+		margin-top: 12px;
+		height: 15px;
+		font-size: 12px;
+		a {
+			color: #77BFED;
+		}
+	}
+
+	button {
+		width: calc(100% - 10px);
+		height: 52px;
+		background: #77BFED;
+		border: none;
+		color: white;
+		margin-top: 22px;
+		font-size: 16px;
+	}
+
+	input {
+		border-bottom: solid 1px #E6E6E6;
+		border-top: none;
+		border-left: none;
+		border-right: none;
+		height: 30px;
+		width: calc(100% - 10px);
+		text-align: center;
+		padding: 6px 0 2px;
+		margin-bottom: 4px;
+		outline: none;
+		font-size: 14px;
+		box-sizing: content-box;
+
+		&:focus {
+			border-bottom: solid 1px #77BFED;
+		}
+	}
+}
+</style>
---
+++ t190701/anctradate/node_modules/vue-auth-component/src/tabs/tabs.vue
@@ -0,0 +1,63 @@
+<template>
+<ul class="vue-auth-component-tabs">
+  <li :class="['signup', signupActive]" @click.prevent="onClick('signup')">
+    <a href="javascript://">Signup</a>
+  </li>
+  <li :class="['login', loginActive]" @click.prevent="onClick('login')">
+    <a href="javascript://">Login</a>
+  </li>
+</ul>
+</template>
+
+<script>
+export default {
+  computed: {
+    signupActive () {
+      return this.activeTab === 'signup' ? 'active' : ''
+    },
+    loginActive () {
+      return this.activeTab === 'login' ? 'active' : ''
+    }
+  },
+  props: {
+    activeTab: {
+      type: String,
+      required: true
+    },
+    onClick: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="less">
+ul.vue-auth-component-tabs {
+  list-style: none;
+  padding: 0;
+  margin: 0 auto;
+  display: flex;
+  font-size: 16px;
+  font-weight: 400;
+
+  li {
+    height: 56px;
+    line-height: 56px;
+    width: 50%;
+    text-align: center;
+    &:not(.active){
+      background: #77BFED;
+      a {
+        color: white;
+        display:block;
+        height: 100%;
+        width: 100%;
+      }
+    }
+  }
+  a:active, a:focus, a:hover, a:visited {
+    text-decoration: none;
+  }
+}
+</style>
