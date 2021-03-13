cc8e76343a3d9673028af6a41676da11fa53c35d
Dexo215 <31929241+Dexa215@users.noreply.github.com>
2021-03-13 21:34:28 +0100

    $store - gestione BAR bg/sm: nei componenti :CLASS

--- t190701/anctradate/frontIE/anc-app/src/components/AncAppBars/B4/r0/c0dx/c0dx.vue
+++ t190701/anctradate/frontIE/anc-app/src/components/AncAppBars/B4/r0/c0dx/c0dx.vue
@@ -1,7 +1,9 @@
 <template>
   <!-- MACRO COLONNA DESTRA ************************************************************************************************************ -->
   <v-col :class="rcD">
+    <!-- test 2021 03 13
     <r1></r1>
+    -->
   </v-col>
 </template>
 <script>
@@ -13,10 +15,31 @@ export default {
   },
   computed: {
     /* DINAMIC CSS */
-    rcD () {
-      return this.$store.getters.rcD;
+    /* rcD(){return this.$store.getters.rcD;},*/
+    rcD() {
+      var rcD = this.$store.getters.rcD;
+      var bb = this.$store.getters.getbarBIG;
+      if (bb === true) {
+        rcD += " BBclass ANCbrd";
+      } else {
+        rcD += " SSclass ANCbrd";
+      }
+      return rcD;
     }
   }
 };
 </script>
-<style media="screen"></style>
+<style media="screen" scoped>
+.BBclass {
+  height: 98%;
+
+  border-radius: 160px 80px 80px 0px;
+  transition: height 0.3s cubic-bezier(0.1, 0.1, 0.1, 0.5), border-radius 0.5s;
+}
+.SSclass {
+  /*background-color: yellowgreen !important;*/
+  height: 75px;
+  border-radius: 48px 48px 48px 48px;
+  transition: height 0.8s, background-color 2s, border-radius 0.5s;
+}
+</style>
